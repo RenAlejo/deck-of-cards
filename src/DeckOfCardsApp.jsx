2e4006e021846994dbe8a334f37248940f8f6935
Jhant23 <jhant1@mail.ccsf.edu>
2022-08-01 21:37:18 -0500

    Initial commit

---
+++ src/DeckOfCardsApp.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function DeckOfCardsApp() {
+
+  return (
+    <>
+    
+    </>
+  );
+
+}
+
+export default DeckOfCardsApp
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DeckOfCardsApp from './DeckOfCardsApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DeckOfCardsApp />
+  </React.StrictMode>
+)
